d3b9b650db8a9641a0e60b6477f972dde58b2f56
Fagner Cruz <fagner@sti.ufpb.br>
2023-09-12 13:16:04 -0300

    exercício 3  - criando navlinks

--- exercicio-rotas/src/components/HeaderBar/index.tsx
+++ exercicio-rotas/src/components/HeaderBar/index.tsx
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
 import "./style.css";
+import { NavLink } from "react-router-dom";
 
 const HeaderBar = () => {
   return (
     <header className="header-container">
-      <nav className="nav-container">
-        <Link to="/">
-          <h1 className="h1link">LOGO</h1>
-        </Link>
+      <Link to="/">
+        <h1 className="h1link">LOGO</h1>
+      </Link>
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "activeLink" : "normalLink")}
+        >
+          Início
+        </NavLink>
+        <NavLink
+          to="/promo"
+          className={({ isActive }) => (isActive ? "activeLink" : "normalLink")}
+        >
+          {" "}
+          Promoção
+        </NavLink>
+        <NavLink
+          to="/inscricao"
+          className={({ isActive }) => (isActive ? "activeLink" : "normalLink")}
+        >
+          {" "}
+          Participar
+        </NavLink>
       </nav>
     </header>
   );
